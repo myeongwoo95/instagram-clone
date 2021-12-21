fa1ee3696e103ae0c6ff70980843c94bd48817db
walle <dlauddn95@gmail.com>
2021-12-22 05:21:57 +0900

    likes, unlikes API

--- src/main/java/com/cos/photogramstart/domain/likes/LikesRepository.java
+++ src/main/java/com/cos/photogramstart/domain/likes/LikesRepository.java
@@ -1,7 +1,17 @@
 package com.cos.photogramstart.domain.likes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LikesRepository extends JpaRepository<Likes, Integer>{
 
+	// 이게 가끔 JPA를 안쓰는게 더 편한 이유다. Likes를 save하기 위해서는 이미지 객체와 유저 객체를 셀렉트해서 넣어줘야 한다...
+	@Modifying
+	@Query(value="insert into likes(imageId, userId, createDate) values(:imageId, :principalId, now())", nativeQuery = true)
+	int mLikes(int imageId, int principalId);
+	
+	@Modifying
+	@Query(value="delete from likes where imageId = :imageId and userId = :principalId", nativeQuery = true)
+	int mUnLikes(int imageId, int principalId);
 }
--- src/main/java/com/cos/photogramstart/service/LikeService.java
+++ src/main/java/com/cos/photogramstart/service/LikeService.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.likes.LikesRepository;
 
@@ -11,4 +12,15 @@ import lombok.RequiredArgsConstructor;
 public class LikeService {
 	
 	private final LikesRepository likesRepository;
+	
+	@Transactional
+	public void 좋아요(int imageId, int principalId) {
+		likesRepository.mLikes(imageId, principalId);
+		
+	}
+	
+	@Transactional
+	public void 좋아요취소(int imageId, int principalId) {
+		likesRepository.mUnLikes(imageId, principalId);
+	}
 }
