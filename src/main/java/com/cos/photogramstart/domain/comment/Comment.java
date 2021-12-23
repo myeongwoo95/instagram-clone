fe0e00952e3d2d94599c7672ac3feb693e340c3c
walle <dlauddn95@gmail.com>
2021-12-23 18:02:45 +0900

    comment domain modeling

---
+++ src/main/java/com/cos/photogramstart/domain/comment/Comment.java
@@ -0,0 +1,51 @@
+package com.cos.photogramstart.domain.comment;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor 
+@NoArgsConstructor
+@Data
+@Entity // @Entity가 붙은 클래스는 JPA가 관리하는 클래스로, 해당 클래스를 엔티티라고 한다. @Entity는 DB에 테이블을 자동으로 생성해준다.
+public class Comment {
+	
+	@Id // primary key 설정, 없으면 에러
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.(mysql = autoincreasement, oracle= sequence)
+	private int id;
+	
+	@Column(length = 100, nullable = false)
+	private String content;
+	
+	@JoinColumn(name ="userId")
+	@ManyToOne
+	private User user;
+	
+	@JoinColumn(name ="imageId")
+	@ManyToOne
+	private Image image;
+	
+	
+	private LocalDateTime createDate;
+	@PrePersist // @PrePersist는 DB에 Insert 되기 직전에 실행
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+
+}
---
+++ src/main/java/com/cos/photogramstart/domain/comment/CommentRespository.java
@@ -0,0 +1,7 @@
+package com.cos.photogramstart.domain.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRespository extends JpaRepository<Comment, Integer>{
+
+}
